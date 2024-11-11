260d3f4f6bfbad52689784612fc649960fe906bf
claudia-netizen <your-email@example.com>
2024-11-11 14:35:37 +0300

    first commit

--- gymapp/urls.py
+++ gymapp/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from gymapp import views
 
 urlpatterns=[
-    path('',views.index,name='index'),
+    path('',views.home,name='home'),
     path('contact',views.contact,name='contact'),
     path('trainer',views.trainer,name='trainer'),
     path('why',views.why,name='why'),
